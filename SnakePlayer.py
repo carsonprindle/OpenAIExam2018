aa6c3fb51a84974dfd02cfbc92b64d80e04e4753
carsonprindle <carson_prindle18@milton.edu>
2018-01-26 14:48:44 -0500

    Final

--- SnakePlayer.py
+++ SnakePlayer.py
@@ -9,6 +9,9 @@ class RandomPlayer():
 
 	### RANDOM ACTION PLAYER ###    
 	def __init__(self, actions):
+		self.totalscore = 0
+		self.runs = 0
+
 		self.actions = actions
 		self.steps = 0
 		self.scores = []
@@ -17,17 +20,19 @@ class RandomPlayer():
 	def over(self):
 		self.steps+=1
 		self.runscore-= 1/self.steps
-		print("Steps to death: {}, Run Score: {}".format(self.steps,self.runscore))
+		#print("Steps to death: {}, Run Score: {}".format(self.steps,self.runscore))
 		self.scores.append(self.runscore)
+		self.runs+=1
 		self.runscore=0
 		self.steps=0
 
 	def testenv(self, reward):
 		self.steps+=1
 		if reward>0:
-			print("Steps to score: {}".format(self.steps))
+			#print("Steps to score: {}".format(self.steps))
 			self.runscore+=reward/self.steps
-			self.steps = 0			
+			self.steps = 0
+			self.totalscore+=1			
 
 	def pickAction(self, reward, obs):
 		self.testenv(reward)
@@ -42,6 +47,14 @@ def sigmoid(x):
 class LearningPlayer():
 
 	def __init__(self, actions):
+		self.i = 0
+		self.plus  = 1
+		self.minus = 1
+		self.pathr = 0.1
+
+		self.totalscore = 0
+		self.runs = 0
+
 		self.steps = 0
 		self.path = []
 		self.runscore = 0
@@ -72,21 +85,32 @@ class LearningPlayer():
 
 	def over(self):
 		self.steps+=1
-		self.runscore-= 1/self.steps
-		print("Steps to death: {}, Run Score: {}".format(self.steps,self.runscore))
-		print(self.path)
-		for p in self.path:
-			self.Q[p[0][0],p[0][1],p[0][2],p[0][3]][p[1]]+=self.runscore
-			print(p[0])
+		self.runscore-= self.minus/self.steps
+		#print("Steps to death: {}, Run Score: {}".format(self.steps,self.runscore))
+		#print(self.path)
+		self.i = 0
+		self.savepath()
+			#print("Path Adjustment: " + str(self.Q[p[0][0],p[0][1],p[0][2],p[0][3]]))
+			#print(p[0])
+		self.runs+=1
 		self.runscore=0
 		self.steps=0
+
+	def savepath(self):
+		for p in self.path:
+			self.i+=1
+			self.Q[p[0][0],p[0][1],p[0][2],p[0][3]][p[1]]+=self.runscore*(1/self.i)
+		path = []
 
 	def testenv(self, reward):
 		self.steps+=1
 		if reward>0:
-			print("Steps to score: {}".format(self.steps))
-			self.runscore+=reward/self.steps
+			#print("Steps to score: {}".format(self.steps))
+			self.totalscore+=1
+			self.runscore+=self.plus/self.steps
 			self.steps = 0
+			self.savepath()
+
 
 	def findState(self):
 		return [int(game.getGameState()["snake_head_x"]),int(game.getGameState()["snake_head_y"]),int(game.getGameState()["food_x"]),int(game.getGameState()["food_x"])]
@@ -96,8 +120,12 @@ class LearningPlayer():
 
 	def pickAction(self, reward, obs):
 		self.testenv(reward)
-		print(str(self.findState()))
-		self.act = max(self.findQ(self.findState()), key=self.findQ(self.findState()).get)
+		#print(str(self.findState()))
+		self.slightRandDic = {}
+		for i in self.findQ(self.findState()):
+			self.slightRandDic[i] = self.findQ(self.findState())[i]+self.pathr*np.random.randint(-10,10)*self.steps
+		#print(self.slightRandDic)
+		self.act = max(self.slightRandDic, key=self.slightRandDic.get)
 		self.rememberStep(self.act)
 		return self.act #highest value action		
 
@@ -110,12 +138,12 @@ game = Snake(
 fps = 30  # fps we want to run at
 frame_skip = 2
 num_steps = 1
-force_fps = False  # True == MegaSpeed
+force_fps = True # True == MegaSpeed
 display_screen = True
 
 reward = 0.0
 max_noops = 20
-nb_frames = 15000
+nb_frames = 1500000
 
 # make a PLE instance.
 p = PLE(game, fps=fps, frame_skip=frame_skip, num_steps=num_steps,
@@ -134,6 +162,13 @@ for i in range(nb_frames):
 	obs = p.getScreenRGB()
 	action = agent.pickAction(reward, obs)
 	reward = p.act(action) #gain in score since last frames
+	#print(i)
+	if ((i+1) % 5000) == 0:
+		print(i+1)
+		print("Average score: " + str(agent.totalscore/agent.runs))
 
-	if i % 120 == 0:
-		p.saveScreen("screen_capture.png")
+	if ((i+1) % 100000) == 0:
+		print("Slow")
+		print("Average score: " + str(agent.totalscore/agent.runs))
+		p.force_fps=False
+		
