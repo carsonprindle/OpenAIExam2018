3e409ac9fc12c349f7721c5069fa55426369a2ac
carsonprindle <carson_prindle18@milton.edu>
2018-01-26 13:01:50 -0500

    Reinforcement First Step

--- SnakePlayer.py
+++ SnakePlayer.py
@@ -5,37 +5,32 @@ import numpy as np
 
 act_values = {}
 
-class HardCodedPlayer():
-
-	def __init__(self, actions):
-		self.actions = actions
-		print(actions)
-		print(game.getGameState())
-			#'snake_head_x','snake_head_y','food_x','food_y','snake_body','snake_body_pos'
-
-	def pickAction(self, reward, obs):
-		if(game.getGameState()["snake_head_y"] < game.getGameState()["food_y"]): #move down
-			return 115  
-		elif(game.getGameState()["snake_head_y"] > game.getGameState()["food_y"]): #move up
-			return 119  
-		elif(game.getGameState()["snake_head_x"] > game.getGameState()["food_x"]): #move left
-			return 97   
-		elif(game.getGameState()["snake_head_x"] < game.getGameState()["food_x"]): #move right
-			return 100  
-		
-		return "none"
-		#119 up
-		#97  left
-		#100 right
-		#115 down
-
 class RandomPlayer():
 
 	### RANDOM ACTION PLAYER ###    
 	def __init__(self, actions):
 		self.actions = actions
+		self.steps = 0
+		self.scores = []
+		self.runscore = 0
+
+	def over(self):
+		self.steps+=1
+		self.runscore-= 1/self.steps
+		print("Steps to death: {}, Run Score: {}".format(self.steps,self.runscore))
+		self.scores.append(self.runscore)
+		self.runscore=0
+		self.steps=0
+
+	def testenv(self, reward):
+		self.steps+=1
+		if reward>0:
+			print("Steps to score: {}".format(self.steps))
+			self.runscore+=reward/self.steps
+			self.steps = 0			
 
 	def pickAction(self, reward, obs):
+		self.testenv(reward)
 		return self.actions[np.random.randint(0, len(self.actions))]
 
 score_scaling = 0.01
@@ -47,22 +42,20 @@ def sigmoid(x):
 class LearningPlayer():
 
 	def __init__(self, actions):
-		self.dist = (200,200)
+		self.steps = 0
+		self.path = []
+		self.runscore = 0
+		self.Q={}
 		self.actions = actions
-		self.act_values = {}
-		for act in actions:
-			act_values[act] = 1.0
-		print(act_values)
+		for sx in range(20):
+			for sy in range(20):
+				for fx in range(20):
+					for fy in range(20):
+						self.Q[sx,sy,fx,fy] = {}
+						for act in actions:
+							self.Q[sx,sy,fx,fy][act] = 1.0
+		print("1111 action vals: " + str(self.Q[1,1,1,1]))
 		self.act = actions[0]
-	
-	def interpretDist(self):
-		distx = game.getGameState()["food_x"]-game.getGameState()["snake_head_x"]
-		disty = game.getGameState()["food_y"]-game.getGameState()["snake_head_y"]
-		return(distx,disty)
-
-	def hyp(self,t):
-		self.straightDist = np.sqrt(np.square(t[0]) + np.square(t[1]))
-		return self.straightDist
 
 	def modVals(self):
 		self.lastAct = self.act
@@ -73,14 +66,44 @@ class LearningPlayer():
 			act_values[self.lastAct]-=score_scaling
 		self.dist = self.interpretDist()
 
+	def rememberStep(self, act):
+		self.path.append((self.findState(),act))
+		#print(self.path)
+
+	def over(self):
+		self.steps+=1
+		self.runscore-= 1/self.steps
+		print("Steps to death: {}, Run Score: {}".format(self.steps,self.runscore))
+		print(self.path)
+		for p in self.path:
+			self.Q[p[0][0],p[0][1],p[0][2],p[0][3]][p[1]]+=self.runscore
+			print(p[0])
+		self.runscore=0
+		self.steps=0
+
+	def testenv(self, reward):
+		self.steps+=1
+		if reward>0:
+			print("Steps to score: {}".format(self.steps))
+			self.runscore+=reward/self.steps
+			self.steps = 0
+
+	def findState(self):
+		return [int(game.getGameState()["snake_head_x"]),int(game.getGameState()["snake_head_y"]),int(game.getGameState()["food_x"]),int(game.getGameState()["food_x"])]
+
+	def findQ(self, fs):
+		return self.Q[fs[0],fs[1],fs[2],fs[3]]
+
 	def pickAction(self, reward, obs):
-		self.act = max(act_values, key=act_values.get)
-		self.modVals()
+		self.testenv(reward)
+		print(str(self.findState()))
+		self.act = max(self.findQ(self.findState()), key=self.findQ(self.findState()).get)
+		self.rememberStep(self.act)
 		return self.act #highest value action		
 
 game = Snake(
-	width = 200,
-	height = 200,
+	width = 20,
+	height = 20,
 	init_length = 3
 )
 
@@ -106,6 +129,7 @@ p.init()
 for i in range(nb_frames):
 	if p.game_over():
 		p.reset_game()
+		agent.over()
 
 	obs = p.getScreenRGB()
 	action = agent.pickAction(reward, obs)
